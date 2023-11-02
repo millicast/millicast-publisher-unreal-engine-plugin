09b1cbf97fc48ac11cc3d848a19b5b1260014155
Grant Wilk <grant@skypadusa.com>
2023-11-02 11:52:53 -0500

    Fix public includes for broken 5.1.0 build

--- Source/MillicastPublisher/MillicastPublisher.Build.cs
+++ Source/MillicastPublisher/MillicastPublisher.Build.cs
@@ -109,7 +109,12 @@ namespace UnrealBuildTool.Rules
 					"d3d11.lib",
 				});
 
-                PrivateIncludePaths.AddRange(new string[] {
+				PublicIncludePaths.AddRange(
+				new string[] {
+					Path.Combine(ModuleDirectory, "Private")
+				});
+
+				PrivateIncludePaths.AddRange(new string[] {
 					"MillicastPublisher/Private",
                     Path.Combine(Path.GetFullPath(Target.RelativeEnginePath), "Source/ThirdParty/WebRTC/4147/Include/third_party/libyuv/include"), // for libyuv headers
 				});
